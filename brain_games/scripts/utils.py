2b72ad42f482044efc4452c30c75ea5b8779480b
yllen32 <furtov1995@yandex.ru>
2023-06-02 02:14:39 +0300

    game logic moved in to utils

--- brain_games/scripts/utils.py
+++ brain_games/scripts/utils.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Callable
 
 import prompt
 
@@ -8,6 +9,7 @@ class Answer(Enum):
     WRONG = 'no'
 
 
+MAX_ATTEMPTS = 3
 CORRECT_STATUS = 'Correct!'
 
 
@@ -39,3 +41,27 @@ def cheer_up(user_name: str) -> None:
 def congratulations_user(user_name: str) -> None:
     """Print a congratulations message."""
     print(f"Congratulations, {user_name}!")
+
+
+def play_game(
+        print_rules: Callable,
+        getting_question: Callable,
+        get_right_answer: Callable,
+        ) -> None:
+    """Play a game with the user, prompting them with a question
+    and checking their answer."""
+    user_name = greeting_user()
+    print_rules()
+    attempts = 0
+    while attempts != MAX_ATTEMPTS:
+        question = getting_question()
+        user_answer = prompt.string(f'Question: {question} ')
+        print(f'Your answer: {user_answer}')
+        right_answer = get_right_answer(question)
+        if user_answer == right_answer:
+            print(CORRECT_STATUS)
+        else:
+            say_user_wrong_answer(user_answer, right_answer)
+            return cheer_up(user_name)
+        attempts += 1
+    return congratulations_user(user_name)
