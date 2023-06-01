27136c294a2dbfa022a502744700c3ef50762489
yllen32 <furtov1995@yandex.ru>
2023-06-01 22:06:51 +0300

    greeting extend by prompt

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+import prompt
+
 
 def main() -> None:
     """Greeting"""
     print('Welcome to the Brain Games!')
+
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
 
 
 if __name__ == '__main__':
