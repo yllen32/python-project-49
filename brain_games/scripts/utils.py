020ba16de1688768a645498f7f2b0b040444e894
yllen32 <furtov1995@yandex.ru>
2023-06-02 01:03:37 +0300

    utils extended by new functions and enums

--- brain_games/scripts/utils.py
+++ brain_games/scripts/utils.py
@@ -1,4 +1,14 @@
+from enum import Enum
+
 import prompt
+
+
+class Answer(Enum):
+    CORRECT = 'yes'
+    WRONG = 'no'
+
+
+CORRECT_STATUS = 'Correct!'
 
 
 def greeting_user() -> str:
@@ -8,3 +18,24 @@ def greeting_user() -> str:
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
+
+
+def say_user_wrong_answer(
+        user_answer: str | int,
+        right_answer: str | int,
+        ) -> None:
+    """Print user answer and correct answer."""
+    return print(
+                f"'{user_answer}' is wrong answer ;(. "
+                f"Correct answer was '{right_answer}'."
+            )
+
+
+def cheer_up(user_name: str) -> None:
+    """Print cheer_up if user lost."""
+    print(f"Let's try again, {user_name}!")
+
+
+def congratulations_user(user_name: str) -> None:
+    """Print a congratulations message."""
+    print(f"Congratulations, {user_name}!")
