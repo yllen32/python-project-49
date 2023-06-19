f98c8c52608fb1963d1785c4ec58049416ab9d56
yllen32 <furtov1995@yandex.ru>
2023-06-19 23:19:18 +0300

    add function for check if the digit is even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -12,10 +12,15 @@ MAX_VALUE = 100
 
 
 def _get_question_and_answer() -> tuple:
-    """Get random integer from"""
+    """Get random integer as question and answer for the question."""
     digit = randint(MIN_VALUE, MAX_VALUE)
-    answer = engine.CORRECT if (digit % 2 == 0) else engine.WRONG
+    answer = engine.CORRECT if _is_even(digit) else engine.WRONG
     return digit, answer
+
+
+def _is_even(digit: int) -> bool:
+    """Return True if digit is even"""
+    return digit % 2 == 0
 
 
 def play_even() -> None:
